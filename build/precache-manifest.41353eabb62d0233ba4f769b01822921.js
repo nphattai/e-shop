d8bbbfea8d3a5f1055317668c9b0347330a699ef
Phát Tài Nguyễn <nphattai98@gmail.com>
2020-02-02 12:11:29 +0700

    add build folder

---
+++ build/precache-manifest.41353eabb62d0233ba4f769b01822921.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6971cbfe56c494d82dad74fec7b7c853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da79d31e7f8641480274",
+    "url": "/static/css/main.898439c3.chunk.css"
+  },
+  {
+    "revision": "e71656a8bc7f7e67c285",
+    "url": "/static/js/2.1df470a7.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.1df470a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da79d31e7f8641480274",
+    "url": "/static/js/main.3c01e7c5.chunk.js"
+  },
+  {
+    "revision": "9d34c6d4dde656c697c9",
+    "url": "/static/js/runtime-main.7773c3e3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
